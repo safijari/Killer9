ad12ef99c22e7a63d996712d752d5c353659f674
Jariullah <safijari@isu.edu>
2022-05-18 08:52:33 -0500

    Initial changes

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+import subprocess
+
 class Plugin:
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def method_1(self, *args):
-        pass
-
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
-    async def method_2(self, *args):
-        pass
+    async def pkill(self, name):
+        return subprocess.Popen("pkill " + str(name), stdout=subprocess.PIPE, shell=True).wait()
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
